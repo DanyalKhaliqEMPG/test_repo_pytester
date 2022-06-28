bc89674509c5ba7b76fe2a350ff09a02a3179e99
Daniyal Khaliq <daniyal.khaliq@empglabs.com>
2022-06-28 18:40:58 +0500

    changed yml file

--- test/test_main.py
+++ test/test_main.py
@@ -4,7 +4,7 @@ import main
 def test_add():
     assert main.add(3, 4) == 7
     assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
+    assert main.add(3.9, 4) == 71
     assert main.add(3.9, 4.1) == 81
 
 
